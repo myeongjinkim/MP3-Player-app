b4b69858eb2877966a37847b3414571a6399a3b7
60142244_김명진 <kiakd2@gmail.com>
2019-12-11 23:49:56 +0900

    remake ndk

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,26 @@
 #include <jni.h>
 #include <string>
 
+
+
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_hw_14r_MainActivity_stringFromJNI(
+Java_com_example_hw_14r_ui_home_HomeFragment_changeTime(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject /* this */ int intTime, jint int_time) {
+
+
+    std::string min;
+    std::string sec;
+    int_time = int_time/1000;
+    if((int_time/60)<10){
+        min = "0"+(int_time/60);
+    }else{
+        min = (int_time/60);
+    }
+    if((int_time%60)<10){
+        sec= "0"+(int_time%60);
+    }else{
+        sec= (int_time%60);
+    }
+    return reinterpret_cast<jstring>(std::min);
 }
